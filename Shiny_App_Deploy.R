6a80fcbcd2deb72cdb1a6608d8362b1f1a2319de
Sivagami <siva1998.16@gmail.com>
2024-12-23 21:38:56 +0000

    Deployment Code

---
+++ Shiny_App_Deploy.R
@@ -0,0 +1,6 @@
+#install.packages("rsconnect")
+#rsconnect::setAccountInfo(name='siva47', token='230C5FC018185F3876E2260E1BBD7DBE',secret='wvHrPbMwycP+VPF1ZcLUDm7r4A+jGRiNnub4t39+')
+library(rsconnect)
+rsconnect::deployApp()
+
+##Deploayment Code
